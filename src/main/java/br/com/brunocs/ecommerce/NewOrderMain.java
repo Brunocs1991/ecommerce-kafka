96d9e1b998986afd8cfeafde2abedffd4e9f4f69
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-05-05 22:17:11 -0300

    config project

---
+++ src/main/java/br/com/brunocs/ecommerce/NewOrderMain.java
@@ -0,0 +1,37 @@
+package br.com.brunocs.ecommerce;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
+
+
+        for (int i = 0; i < 3000 ; i++) {
+             var value = "132123,"+i +",7894589754";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+        producer.send(record, (data, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+            }
+            System.out.println(data.topic() + "::: " + data.partition() + "::: " + data.offset() + "/" + data.timestamp());
+        }).get();
+        }
+    }
+
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
